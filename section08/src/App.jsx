7bf30b4f1c310670e08b5994edd5b47d9d883922
이성 <hughlee@idstrust.com>
2025-03-14 07:04:58 +0900

    8.3 기능구현준비하기

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -2,8 +2,31 @@ import "./App.css";
 import Editor from "./components/Editor";
 import Header from "./components/Header";
 import List from "./components/List";
+import { useState } from "react";
+
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: "react공부하기",
+    date: new Date().getTime,
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "밥먹기",
+    date: new Date().getTime,
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "공부하기",
+    date: new Date().getTime,
+  },
+];
 
 function App() {
+  const [todos, setTodos] = useState([mockData]);
   return (
     <div className="App">
       <section>
