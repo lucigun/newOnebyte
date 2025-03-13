238f7bdb859159505ef87e29e85c623cfcb6386d
이성 <hughlee@idstrust.com>
2025-03-14 07:52:28 +0900

    new

--- section08/src/components/Header.jsx
+++ section08/src/components/Header.jsx
@@ -1,11 +1,11 @@
 import "./header.css";
 const Header = () => {
-  return (
-    <div className="Header">
-      <h3>오늘은⭕</h3>
-      <h1>{new Date().toDateString()}</h1>
-    </div>
-  );
+	return (
+		<div className="Header">
+			<h3>오늘은⭕</h3>
+			<h1>{new Date().toDateString()}</h1>
+		</div>
+	);
 };
 
 export default Header;
