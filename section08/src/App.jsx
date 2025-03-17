fcf95fc2cfa08d2c9fd4db512769a5fe043b5218
이성 <hughlee@idstrust.com>
2025-03-18 07:31:08 +0900

    8.7 Delete - 투두삭제하기

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -47,6 +47,11 @@ function App() {
 		);
 	};
 
+	const onDelete = (targetId) => {
+		// 인수로 todos 배열에서 targetId와 일치하는 id를 요소만 삭제한 새로운 배열
+		setTodos(todos.filter((todo) => todo.id !== targetId));
+	};
+
 	return (
 		<div className="App">
 			<section>
@@ -56,7 +61,7 @@ function App() {
 				<Editor onCreate={onCreate} /> {/* onCreate prop 추가 */}
 			</section>
 			<section>
-				<List todos={todos} onUpdate={onUpdate} />
+				<List todos={todos} onUpdate={onUpdate} onDelete={onDelete} />
 			</section>
 		</div>
 	);
--- section08/src/components/TodoItem.jsx
+++ section08/src/components/TodoItem.jsx
@@ -1,15 +1,19 @@
 import "./TodoItem.css";
 
-const TodoItem = ({ id, isDone, content, date, onUpdate }) => {
+const TodoItem = ({ id, isDone, content, date, onUpdate, onDelete }) => {
 	const onChangeCheckbox = () => {
 		onUpdate(id);
+	};
+
+	const onClickDelete = () => {
+		onDelete(id);
 	};
 	return (
 		<div className="TodoItem">
 			<input onChange={onChangeCheckbox} checked={isDone} type="checkbox" />
 			<div className="content">{content}</div>
 			<div className="date">{new Date(date).toLocaleDateString()}</div>
-			<button>삭제</button>
+			<button onClick={onClickDelete}>삭제</button>
 		</div>
 	);
 };
