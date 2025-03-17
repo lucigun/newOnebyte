169867be645926327fadab0ae77dbb22df3cd049
이성 <hughlee@idstrust.com>
2025-03-18 07:20:07 +0900

    8.6 Update-투두수정하기

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -5,52 +5,61 @@ import List from "./components/List";
 import { useState, useRef } from "react";
 
 const mockData = [
-  {
-    id: 0,
-    isDone: false,
-    content: "react공부하기",
-    date: new Date().getTime(),
-  },
-  {
-    id: 1,
-    isDone: false,
-    content: "밥먹기",
-    date: new Date().getTime(),
-  },
-  {
-    id: 2,
-    isDone: false,
-    content: "공부하기",
-    date: new Date().getTime(),
-  },
+	{
+		id: 0,
+		isDone: false,
+		content: "react공부하기",
+		date: new Date().getTime(),
+	},
+	{
+		id: 1,
+		isDone: false,
+		content: "밥먹기",
+		date: new Date().getTime(),
+	},
+	{
+		id: 2,
+		isDone: false,
+		content: "공부하기",
+		date: new Date().getTime(),
+	},
 ];
 
 function App() {
-  const [todos, setTodos] = useState(mockData); // 수정된 부분
-  const idRef = useRef(3);
-  const onCreate = (content) => {
-    const newTodo = {
-      id: idRef.current++,
-      isDone: false,
-      content: content,
-      date: new Date().getTime(),
-    };
-    setTodos([newTodo, ...todos]);
-  };
+	const [todos, setTodos] = useState(mockData); // 수정된 부분
+	const idRef = useRef(3);
+	const onCreate = (content) => {
+		const newTodo = {
+			id: idRef.current++,
+			isDone: false,
+			content: content,
+			date: new Date().getTime(),
+		};
+		setTodos([newTodo, ...todos]);
+	};
 
-  return (
-    <div className="App">
-      <section>
-        <Header />
-      </section>
-      <section>
-        <Editor onCreate={onCreate} /> {/* onCreate prop 추가 */}
-      </section>
-      <section>
-        <List todos={todos} />
-      </section>
-    </div>
-  );
+	const onUpdate = (targetId) => {
+		// todos state의 값에서 targetId를 가진 객체를 찾아서 isDone 값을 반전시킨다.
+		setTodos(
+			todos.map((todo) =>
+				todo.id === targetId ? { ...todo, isDone: !todo.isDone } : todo
+			)
+		);
+	};
+
+	return (
+		<div className="App">
+			<section>
+				<Header />
+			</section>
+			<section>
+				<Editor onCreate={onCreate} /> {/* onCreate prop 추가 */}
+			</section>
+			<section>
+				<List todos={todos} onUpdate={onUpdate} />
+			</section>
+		</div>
+	);
 }
 
 export default App;
--- section08/src/components/List.jsx
+++ section08/src/components/List.jsx
@@ -2,36 +2,36 @@ import "./List.css";
 import TodoItem from "./TodoItem";
 import { useState } from "react";
 
-const List = ({ todos }) => {
-  const [search, setSearch] = useState("");
-  const onChangeSearch = (e) => {
-    setSearch(e.target.value);
-    console.log(e.target.value);
-  };
-  const getFilteredData = (todo) => {
-    if (search === "") {
-      return todos;
-    }
-    return todos.filter((todo) => {
-      return todo.content.toLowerCase().includes(search.toLowerCase());
-    });
-  };
-  const filteredTodos = getFilteredData();
-  return (
-    <div className="List">
-      <h4>todo list</h4>
-      <input
-        value={search}
-        onChange={onChangeSearch}
-        placeholder="검색어 입력"
-      ></input>
-      <div className="todoswrapper">
-        {filteredTodos.map((todo) => {
-          return <TodoItem key={todo.id} {...todo} />;
-        })}
-      </div>
-    </div>
-  );
+const List = ({ todos, onUpdate }) => {
+	const [search, setSearch] = useState("");
+	const onChangeSearch = (e) => {
+		setSearch(e.target.value);
+		console.log(e.target.value);
+	};
+	const getFilteredData = (todo) => {
+		if (search === "") {
+			return todos;
+		}
+		return todos.filter((todo) => {
+			return todo.content.toLowerCase().includes(search.toLowerCase());
+		});
+	};
+	const filteredTodos = getFilteredData();
+	return (
+		<div className="List">
+			<h4>todo list</h4>
+			<input
+				value={search}
+				onChange={onChangeSearch}
+				placeholder="검색어 입력"></input>
+			<div className="todoswrapper">
+				{filteredTodos.map((todo) => {
+					// return <TodoItem key={todo.id} {...todo} onUpdate={onUpdate}/>;
+					return <TodoItem key={todo.id} {...todo} onUpdate={onUpdate} />; // onUpdate prop 추가
+				})}
+			</div>
+		</div>
+	);
 };
 
 export default List;
