f00cd1626373d0482550fbaebbb578433a3a1e08
이성 <hughlee@idstrust.com>
2025-03-19 07:50:01 +0900

    10.2 useMemo

--- section10/src/components/List.jsx
+++ section10/src/components/List.jsx
@@ -1,6 +1,6 @@
 import "./List.css";
 import TodoItem from "./TodoItem";
-import { useState } from "react";
+import { useState, useMemo } from "react";
 
 const List = ({ todos, onUpdate, onDelete }) => {
 	const [search, setSearch] = useState("");
@@ -17,9 +17,31 @@ const List = ({ todos, onUpdate, onDelete }) => {
 		});
 	};
 	const filteredTodos = getFilteredData();
+
+	const { totalCount, doneCount, notDoneCount } = useMemo(() => {
+		console.log("getAnalyzeData호출");
+		const totalCount = todos.length;
+		const doneCount = todos.filter((todo) => todo.isDone).length;
+		const notDoneCount = totalCount - doneCount;
+		return {
+			totalCount,
+			doneCount,
+			notDoneCount,
+		};
+	}, [todos]);
+	//  [] <= 의존성배열 : deps
+	// deps에 있는 값이 변경될때만 함수가 호출된다.
+
+	// const { totalCount, doneCount, notDoneCount } = getAnalyzeData();
+
 	return (
 		<div className="List">
 			<h4>todo list</h4>
+			<div>
+				<div>total : {totalCount}</div>
+				<div>done : {doneCount}</div>
+				<div>notDone : {notDoneCount}</div>
+			</div>
 			<input
 				value={search}
 				onChange={onChangeSearch}
