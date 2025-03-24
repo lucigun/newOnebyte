4354ce72d06ad291d73fa057d82381aede428c61
이성 <hughlee@idstrust.com>
2025-03-25 07:25:32 +0900

    12.3 페이지라우팅설정하기

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Diary from "./pages/Diary";
+import New from "./pages/New";
+import NotFound from "./pages/Notfound";
+
+// 1. "/" : 모든 일기 조회하는 Home
+// 2. "/new" : 새 일기 작성하는 페이지
+// 3. "/diary" : 일기를 조회하는 view 페이지
+
+// Routes 컴퍼넌트 내에는 Route 컴퍼넌트만 들어갈 수 있음
+// Routes 컴퍼넌트 밖에 추가하는 경우 모든 페이지에 적용됨
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/new" element={<New />} />
+				<Route path="/diary" element={<Diary />} />
+				<Route path="*" element={<NotFound />} />
+			</Routes>
+		</>
+	);
 }
 
-export default App
+export default App;
